6b20b0d7b91e09b48bbf78fd460cdcb82682e674
brandon-debug <brandon.j.p.braham29@gmail.com>
2026-06-25 02:21:44 -0500

    Add files via upload
    First stage of my F1 Sim project. Starting off with creating the core database with team and driver info in nested dictionaries.

---
+++ F1 Project.py
@@ -0,0 +1,255 @@
+#--------------F1 App V1-------------------#
+
+Mercedes = {
+    "name": "Mercedes",
+    "nationality": "German",
+    "cc_position": 2,
+    "team_rating": 90,
+    "drivers": [
+        {
+            "name": "George Russell",
+            "number": 63,
+            "nationality": "British",
+            "wdc_position": 4,
+            "driver_rating": 89
+        },
+        {
+            "name": "Kimi Antonelli",
+            "number": 12,
+            "nationality": "Italian",
+            "wdc_position": 7,
+            "driver_rating": 77
+        }
+    ]
+}
+
+McLaren = {
+    "name": "McLaren",
+    "nationality": "British",
+    "cc_position" : 1,
+    "team_rating" : 99,
+    "drivers" : [
+        {
+            "name" : "Lando Norris",
+            "number" : 1,
+            "nationality" : "British",
+            "wdc-position" : 1,
+            "driver_rating": 98,
+        },
+        {
+            "name" : "Oscar Piastri",
+            "number" : 81,
+            "nationality" : "Australian",
+            "wdc-position" : 3,
+            "driver_rating": 94,
+        }
+    ]
+}
+
+Redbull = {
+    "name": "Redbull Racing",
+    "nationality": "Austrian",
+    "cc_position": 3,
+    "team_rating": 90,
+    "drivers": [
+        {
+            "name": "Max Verstappen",
+            "number": 3,
+            "nationality": "Dutch",
+            "wdc_position": 2,
+            "driver_rating": 98
+        },
+        {
+            "name": "Issac Hadjar",
+            "number": 6,
+            "nationality": "French",
+            "wdc_position": 12,
+            "driver_rating": 70
+        }
+    ]
+}
+
+Ferrari = {
+    "name": "Ferrari",
+    "nationality": "German",
+    "cc_position": 4,
+    "team_rating": 80,
+    "drivers": [
+        {
+            "name": "Lewis Hamilton",
+            "number": 44,
+            "nationality": "British",
+            "wdc_position": 6,
+            "driver_rating": 89
+        },
+        {
+            "name": "Charles Leclerc",
+            "number": 16,
+            "nationality": "Italian",
+            "wdc_position": 5,
+            "driver_rating": 75
+        }
+    ]
+}
+
+Aston = {
+    "name": "Aston Martin",
+    "nationality": "British",
+    "cc_position": 7,
+    "team_rating": 70,
+    "drivers": [
+        {
+            "name": "Fernando Alonzo",
+            "number": 14,
+            "nationality": "Spanish",
+            "wdc_position": 10,
+            "driver_rating": 89
+        },
+        {
+            "name": "Lance Stroll",
+            "number": 18,
+            "nationality": "Italian",
+            "wdc_position": 16,
+            "driver_rating": 70
+        }
+    ]
+}
+
+Williams = {
+    "name": "Williams Racing",
+    "nationality": "British",
+    "cc_position": 5,
+    "team_rating": 72,
+    "drivers": [
+        {
+            "name": "Alex Albon",
+            "number": 23,
+            "nationality": "Filipino",
+            "wdc_position": 8,
+            "driver_rating": 74
+        },
+        {
+            "name": "Carlos Sainz",
+            "number": 55,
+            "nationality": "Italian",
+            "wdc_position": 9,
+            "driver_rating": 78
+        }
+    ]
+}
+
+HAAS = {
+    "name": "Toyota Gazoo HAAS Racing",
+    "nationality": "American",
+    "cc_position": 8,
+    "team_rating": 90,
+    "drivers": [
+        {
+            "name": "Esteban Ocon",
+            "number": 31,
+            "nationality": "French",
+            "wdc_position": 15,
+            "driver_rating": 89
+        },
+        {
+            "name": "Ollie Bearman",
+            "number": 87,
+            "nationality": "British",
+            "wdc_position": 13,
+            "driver_rating": None
+        }
+    ]
+}
+
+Audi = {
+    "name": "Audi",
+    "nationality": "German",
+    "cc_position": 9,
+    "team_rating": 90,
+    "drivers": [
+        {
+            "name": "Nico Hulkenberg",
+            "number": 27,
+            "nationality": "German",
+            "wdc_position": 11,
+            "driver_rating": 89
+        },
+        {
+            "name": "Gabriel Bortolleto",
+            "number": 5,
+            "nationality": "Brazilian",
+            "wdc_position": 19,
+            "driver_rating": None
+        }
+    ]
+}
+
+Cadillac = {
+    "name": "Cadillac Racing",
+    "nationality": "American",
+    "cc_position": None,
+    "team_rating": 90,
+    "drivers": [
+        {
+            "name": "Valterri Bottas",
+            "number": 77,
+            "nationality": "Finnish",
+            "wdc_position": None,
+            "driver_rating": 89
+        },
+        {
+            "name": "Sergio Perez",
+            "number": 11,
+            "nationality": "Mexican",
+            "wdc_position": None,
+            "driver_rating": 76
+        }
+    ]
+}
+
+VCarb = {
+    "name": "Visa Cash App Racing Bulls",
+    "nationality": "German",
+    "cc_position": 6,
+    "team_rating": 90,
+    "drivers": [
+        {
+            "name": "Liam Lawson",
+            "number": 30,
+            "nationality": "New Zealander",
+            "wdc_position": 14,
+            "driver_rating": 89
+        },
+        {
+            "name": "Arvid Linblad",
+            "number": 41,
+            "nationality": "British",
+            "wdc_position": None,
+            "driver_rating": None
+        }
+    ]
+}
+
+Alpine = {
+    "name": "Alpine",
+    "nationality": "French",
+    "cc_position": 10,
+    "team_rating": 60,
+    "drivers": [
+        {
+            "name": "Pierre Gasly",
+            "number": 10,
+            "nationality": "British",
+            "wdc_position": 18,
+            "driver_rating": 89
+        },
+        {
+            "name": "Franco Colapinto",
+            "number": 43,
+            "nationality": "Argentine",
+            "wdc_position": 20,
+            "driver_rating": None
+        }
+    ]
+}
+
